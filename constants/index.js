bc86214c8949baa449e20b41d553598750bc500a
BigBangCrypto <91626244+BigBangCrypto@users.noreply.github.com>
2023-07-21 17:14:28 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x075b3c2bbbba7b2cd6706e922873796432a42a02";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
